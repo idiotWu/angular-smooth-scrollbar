147b7daa357719f3c23e98c2d33b1fdbf86e51da
Dolphin Wood <dolphin.w.e@gmail.com>
2015-12-10 18:34:28 +0800

    chore(gulp): refact workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,106 +1,45 @@
 var gulp = require('gulp');
-var util = require('gulp-util');
-var stylus = require('gulp-stylus');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var replace = require('gulp-replace');
-var webpack = require('webpack-stream');
-var sizereport = require('gulp-sizereport');
-var autoprefixer = require('gulp-autoprefixer');
+var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
+var wrapUMD = require('gulp-wrap-umd');
 var browserSync = require('browser-sync').create();
+var friendlyFormatter = require("eslint-friendly-formatter");
 
-var compile = function(watch, done) {
-    var options = {
-        watch: watch,
-        output: {
-            filename: 'angular-smooth-scrollbar.js'
-        },
-        module: {
-            preLoaders: [{
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'eslint-loader'
-            }],
-            loaders: [{
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader?optional[]=runtime&stage=0'
-            }]
-        }
-    };
-
-    if (watch) {
-        options.devtool = 'inline-source-map';
-    }
-
-    return webpack(options, null, function(err, stats) {
-        if (err) throw new util.PluginError('webpack', err);
-
-        util.log('[webpack]', stats.toString({
-            colors: util.colors.supportsColor,
-            chunks: false,
-            hash: false,
-            version: false
-        }));
-
-        browserSync.reload();
-
-        if (watch) {
-            watch = false;
-            done();
-        }
-    });
-};
-
-gulp.task('scripts:watch', function(done) {
+gulp.task('compile', function() {
     return gulp.src('index.js')
-        .pipe(compile(true, done))
-        .pipe(gulp.dest('build/'));
-});
-
-gulp.task('scripts:build', function() {
-    return gulp.src('index.js')
-        .pipe(compile(false))
-        .pipe(gulp.dest('build/'));
-});
-
-gulp.task('scripts:release', ['scripts:build'], function() {
-    return gulp.src('index.js')
-        .pipe(compile(false))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('replace', function() {
-    return gulp.src('test/index.html')
-        .pipe(replace(/build/g, 'dist'))
-        .pipe(gulp.dest('demo/'));
-});
-
-gulp.task('copy:style', function() {
-    return gulp.src('bower_components/smooth-scrollbar/dist/smooth_scrollbar.css')
-        .pipe(rename({
-            basename: 'angular-smooth-scrollbar'
+        .pipe(babel({
+            presets: ['es2015', 'stage-0']
         }))
-        .pipe(gulp.dest('build/'))
+        .pipe(wrapUMD({
+            exports: 'undefined', // export nothing
+            namespace: 'undefined',
+            deps: [{
+                name: 'angular',
+                globalName: 'angular',
+                paramName: 'angular'
+            }, {
+                name: 'smooth-scrollbar',
+                globalName: 'Scrollbar',
+                paramName: 'Scrollbar'
+            }]
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream())
 });
 
-gulp.task('serve', ['scripts:watch', 'copy:style'], function() {
+gulp.task('lint', function() {
+    return gulp.src('index.js')
+        .pipe(eslint())
+        .pipe(eslint.format(friendlyFormatter));
+});
+
+gulp.task('serve', ['compile', 'lint'], function() {
     browserSync.init({
         server: ['./test', '.']
     });
 
+    gulp.watch('index.js', ['compile', 'lint']);
     gulp.watch('test/*.*').on('change', browserSync.reload);
 });
 
-gulp.task('copy:demo', function() {
-    return gulp.src(['test/**/*.*', '!test/**/*.html'])
-        .pipe(gulp.dest('demo/'));
-});
-
-gulp.task('release', ['scripts:release', 'replace', 'copy:demo'], function() {
-    return gulp.src('dist/**/*.*')
-        .pipe(sizereport());
-});
-
-gulp.task('default', ['release']);
+gulp.task('default', ['serve']);
