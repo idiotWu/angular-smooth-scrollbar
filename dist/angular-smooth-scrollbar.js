02142ae50c6c2625552d086d3ca8c52bf700cbdd
Dolphin Wood <dolphin.w.e@gmail.com>
2016-04-24 03:01:30 +0900

    :tada: Release v5.5.0

--- dist/angular-smooth-scrollbar.js
+++ dist/angular-smooth-scrollbar.js
@@ -144,6 +144,7 @@ angular.module('SmoothScrollbar', []).constant('SCROLLBAR_VERSION', Scrollbar.ve
             speed: '@',
             friction: '@',
             thumbMinSize: '@',
+            renderByPixels: '=',
             continuousScrolling: '=',
             ignoreEvents: '='
         },
